82a38d44d9e1ead3ef8556d7c5ec7a5d51af8f95
Alina Bezzub <alina.ttt000@mail.ru>
2023-09-22 16:25:37 +0300

    add api with names and button view alll

--- script.js
+++ script.js
@@ -35,23 +35,7 @@ countTimer(count3, 'element_2');
 
 
 
-//navigation bar
-const openBtn = document.querySelector('.nav-open')
-const closeBtn = document.querySelector('.nav-close')
-const nav = document.querySelectorAll('.nav')
-
-openBtn.addEventListener('click', () => {
-  nav.forEach(navEl => {
-    navEl.classList.add('visible')
-  })
-})
-
-closeBtn.addEventListener('click', () => {
-  nav.forEach(navEl => {
-    navEl.classList.remove('visible')
-  })
-})
-
+//Slider
 async function setupSlider(sliderClass, rightBtnClass, leftBtnClass) {
   const sliders = [...document.querySelectorAll(sliderClass)];
   const rightBtns = [...document.querySelectorAll(rightBtnClass)];
@@ -71,7 +55,96 @@ async function setupSlider(sliderClass, rightBtnClass, leftBtnClass) {
     });
   });
 }
-
 // Используем функцию для настройки слайдеров и кнопок
 setupSlider('.bid-slider-elem', '.btn-sl-right', '.btn-sl-left');
 setupSlider('.popular-elements', '.arr-right', '.arr-left');
+
+
+const links = ['.nav-link-explore', '.nav-link-creators', '.nav-link-community']
+const targets = ['.explore', '.rating', '.drop']
+
+//Убираем класс
+function removeActiveClass() {
+  links.forEach((link) => {
+    const linkElem = document.querySelector(link);
+    linkElem.classList.remove('nav-link-active');
+  });
+}
+
+//Smooth scroll
+const smoothLinks = (arr1, arr2) => {
+
+  links.forEach((link, idx) => {
+    const linkElem = document.querySelector(link)
+    const targetElem = document.querySelector(targets[idx])
+
+    if(links.length === targets.length){
+      linkElem.addEventListener('click', function(event){
+
+        event.preventDefault()
+        removeActiveClass()
+
+        targetElem.scrollIntoView({behavior: 'smooth'})
+        linkElem.classList.add('nav-link-active')
+      })
+    }
+  });
+}
+smoothLinks(links, targets)
+
+// nav bar
+const openBtn = document.querySelector('.nav-open')
+const closeBtn = document.querySelector('.nav-close')
+const nav = document.querySelectorAll('.nav')
+
+openBtn.addEventListener('click', () => {
+  nav.forEach(navEl => {
+    navEl.classList.add('visible')
+  })
+})
+
+closeBtn.addEventListener('click', () => {
+  closeNavBar()
+})
+ 
+function closeNavBar(){
+  nav.forEach(navEl => {
+    navEl.classList.remove('visible')
+  })
+}
+//To close nav bar when the link was pressed
+const linksNavBar = document.querySelectorAll('.navbar-link')
+
+linksNavBar.forEach(link => {
+  link.addEventListener('click', closeNavBar)
+})
+
+const exploreCard = document.querySelectorAll('.explore-bid')
+const btnViewAll = document.querySelector('.btn-tr2')
+
+let currentIndex = 0
+
+function showCards(){
+  for(let i = currentIndex; i < currentIndex + 3 && i < exploreCard.length; i++){
+    exploreCard[i].classList.remove('hidden')
+  }
+  currentIndex += 3
+
+  if(currentIndex >= exploreCard.length){
+    document.querySelector('.auctions-btn').style.display = 'none'
+  }
+}
+
+btnViewAll.addEventListener('click', showCards)
+showCards()
+
+const userNames = document.querySelectorAll('.rating-elem-info__nickname')
+
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        for(let i = 0; i < json.length; i++){
+          let dataNames = json[i].name
+          userNames[i].innerHTML = dataNames
+        }
+      })
